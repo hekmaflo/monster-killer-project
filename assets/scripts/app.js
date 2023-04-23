0f5855e4a1611ebab67e88d4450f215823e71504
Hector Flores <106941903+hekmaflo@users.noreply.github.com>
2023-04-22 21:04:38 -0500

    added monster attack logic

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -3,12 +3,22 @@ let currentMonsterHealth = chosenMaxLife;
 let currentPlayerHealth = chosenMaxLife;
 
 const ATTACK_VALUE = 10;
+const MONSTER_ATTACK_VALUE = 14;
 
 adjustHealthBars(chosenMaxLife);
 
 const attackHandler = () => {
   const damage = dealMonsterDamage(ATTACK_VALUE);
   currentMonsterHealth -= damage;
+  const monsterDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
+  currentPlayerHealth -= monsterDamage;
+  if (currentMonsterHealth <= 0 && currentPlayerHealth > 0) {
+    alert("You won!");
+  } else if (currentPlayerHealth <= 0 && currentMonsterHealth > 0) {
+    alert("Monster won!");
+  } else if (currentPlayerHealth <= 0 && currentMonsterHealth <= 0) {
+    alert("You have a draw!");
+  }
 };
 
-attackBtn.addEventListener("click", attackHandlerd);
+attackBtn.addEventListener("click", attackHandler);
