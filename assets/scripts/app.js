601b8c904c56399b704b197f852918a434ebc3d4
Hector Flores <106941903+hekmaflo@users.noreply.github.com>
2023-04-22 21:09:32 -0500

    added strong attack value and logic

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -4,6 +4,7 @@ let currentPlayerHealth = chosenMaxLife;
 
 const ATTACK_VALUE = 10;
 const MONSTER_ATTACK_VALUE = 14;
+const STRONG_ATTACK_VALUE = 17;
 
 adjustHealthBars(chosenMaxLife);
 
@@ -21,4 +22,19 @@ const attackHandler = () => {
   }
 };
 
+const strongAttackHandler = () => {
+  const damage = dealMonsterDamage(STRONG_ATTACK_VALUE);
+  currentMonsterHealth -= damage;
+  const monsterDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
+  currentPlayerHealth -= monsterDamage;
+  if (currentMonsterHealth <= 0 && currentPlayerHealth > 0) {
+    alert("You won!");
+  } else if (currentPlayerHealth <= 0 && currentMonsterHealth > 0) {
+    alert("Monster won!");
+  } else if (currentPlayerHealth <= 0 && currentMonsterHealth <= 0) {
+    alert("You have a draw!");
+  }
+};
+
 attackBtn.addEventListener("click", attackHandler);
+strongAttackBtn.addEventListener("click", strongAttackHandler);
